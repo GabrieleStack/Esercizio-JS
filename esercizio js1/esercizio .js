c713fc55c8032b5984161c41eacd2ee082f157b6
GabrieleStack <158114204+GabrieleStack@users.noreply.github.com>
2024-02-09 18:55:03 +0100

    Carico l'esercizio
    Spero sia giusto!

---
+++ esercizio js1/esercizio .js
@@ -0,0 +1,59 @@
+/* //ESERCIZIO 1
+
+let numeri = 5 ("possono essere sia decimali che interi")
+
+let stringhe = "ciao" ("possono essere una sequenza di più caratteri, e vengono scritti all'interno degli apici")
+
+let boolean = true ("servono ad indicare se una varibile è vera(true) o falsa(false) ad esempio: let = n1 = 5, let = n2 = 5, console.log(n1 == n2) = true")
+
+let object = {
+    
+    tipo: "chiaveInglese",
+
+    materiale: "metallo",
+
+    pesante: true
+}
+ ("sono entità che possono contenere diverse caratteristiche e proprietà")
+
+let array1 = [1,2,3,4,5] 
+
+let array2 = ["cane", "gatto", "topo"]
+
+("sono una lista di elementi, come ad esempio di oggetti, e possono contenere sia stringhe, che numeri che boolean, ecc..")
+
+null ("rappresenta l'assenza di un valore, una variabie con valore nullo")
+
+undefined ("rappresenta una variabile che non è stata inizializzata, quindi come se js non capisse dove si vuole arrivare, di conseguenza: Undefined") */
+
+//ESERCIZIO2
+
+let n1 = 12
+
+let n2 = 20
+
+console.log( n1 + n2 );
+
+//ESERCIZIO3
+
+let x = 12
+
+//ESERCIZIO4
+
+let nome = "Gabriele Prospero"
+
+//ESERCIZIO5
+
+let n3 = 4
+
+console.log(n3 - x)
+
+//ESERCIZIO6
+
+let name1 = "jhon"
+
+let name2 = "Jhon"
+
+console.log( name1 !== name2);
+console.log( name1 === name2.toLowerCase())
+
